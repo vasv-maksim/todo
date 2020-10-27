1517039a276a04f7e10b734bf7921a99fd2c2908
vasev maksim <vasvmaksim@gmail.com>
2020-10-27 15:56:08 +0300

    config environment

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { Routes, RouterModule } from '@angular/router';
-
 
 const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule { }
