ee67336b18db76ccf8550576c7ba2de327ce01ac
vasev maksim <vasvmaksim@gmail.com>
2020-10-28 18:40:01 +0300

    todo add task model

--- front/src/app/todo-module/tasks/tasks.component.ts
+++ front/src/app/todo-module/tasks/tasks.component.ts
@@ -1,25 +1,63 @@
 import { Component } from '@angular/core';
 import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 
+import { Task } from '../common/models';
+
 @Component({
   selector: 'todo-tasks',
   templateUrl: './tasks.component.html',
   styleUrls: ['./tasks.component.scss'],
 })
 export class TasksComponent {
-  tasks: string[] = [
-    'Найти ананас',
-    'Выбрать ананас',
-    'Купить ананас',
-    'Донести ананас',
-    'Положить ананас в холодильник',
-    'Достать ананас из холодильника',
-    'Почистить ананас',
-    'Разрезать ананас',
-    'Сесть ананас',
+  tasks: Task[] = [
+    {
+      id: 1,
+      name: 'Найти ананас',
+      done: true,
+    },
+    {
+      id: 2,
+      name: 'Выбрать ананас',
+      done: true,
+    },
+    {
+      id: 3,
+      name: 'Купить ананас',
+      done: true,
+    },
+    {
+      id: 4,
+      name: 'Донести ананас',
+      done: false,
+    },
+    {
+      id: 5,
+      name: 'Положить ананас в холодильник',
+      done: false,
+    },
+    {
+      id: 6,
+      name: 'Достать ананас из холодильника',
+      done: false,
+    },
+    {
+      id: 7,
+      name: 'Почистить ананас',
+      done: false,
+    },
+    {
+      id: 8,
+      name: 'Разрезать ананас',
+      done: false,
+    },
+    {
+      id: 9,
+      name: 'Съесть ананас',
+      done: false,
+    },
   ];
 
-  drop(event: CdkDragDrop<string[]>) {
+  drop(event: CdkDragDrop<Task[]>) {
     moveItemInArray(this.tasks, event.previousIndex, event.currentIndex);
   }
 }
