f77e62adaf3f108b140d71718cc18e73346d3941
vasev maksim <vasvmaksim@gmail.com>
2020-10-28 20:04:11 +0300

    customize scrollbar

--- front/src/app/todo-module/todo/todo.component.ts
+++ front/src/app/todo-module/todo/todo.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'todo',
   templateUrl: './todo.component.html',
   styleUrls: ['./todo.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class TodoComponent {}
