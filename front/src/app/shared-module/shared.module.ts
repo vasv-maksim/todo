c9663b884dd2aa3c01accb4a02f657d758513b41
vasev maksim <vasvmaksim@gmail.com>
2020-10-29 20:01:15 +0300

    shared add forms import

--- front/src/app/shared-module/shared.module.ts
+++ front/src/app/shared-module/shared.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  imports: [CommonModule],
-  exports: [CommonModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
+  exports: [CommonModule, FormsModule, ReactiveFormsModule],
 })
 export class SharedModule {}
