a7fd65cca11789053c2fa6ecaed344646cf2737f
vasev maksim <vasvmaksim@gmail.com>
2020-10-29 20:09:18 +0300

    todo change add-dialog

--- front/src/app/todo-module/add-dialog/add-dialog.component.ts
+++ front/src/app/todo-module/add-dialog/add-dialog.component.ts
@@ -1,8 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { MatDialogRef } from '@angular/material/dialog';
+import { v4 as uuid } from 'uuid';
+
+import { addTaskAction } from '../store/todo.action';
+import { Task } from '../store/models';
 
 @Component({
-  selector: 'app-add-dialog',
+  selector: 'todo-add-dialog',
   templateUrl: './add-dialog.component.html',
   styleUrls: ['./add-dialog.component.scss'],
 })
-export class AddDialogComponent implements OnInit {}
+export class AddDialogComponent implements OnInit {
+  taskName: string = 'Новая задача';
+
+  constructor(
+    private store: Store,
+    private dialogRef: MatDialogRef<AddDialogComponent>,
+  ) {}
+
+  ngOnInit(): void {}
+
+  addTask() {
+    const task: Task = { id: uuid(), name: this.taskName, done: false };
+    this.store.dispatch(addTaskAction(task));
+    this.dialogRef.close();
+  }
+
+  cancel() {
+    this.dialogRef.close();
+  }
+}
