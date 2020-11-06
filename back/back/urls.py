4c3a81f07dec8a5f764df6dc51aaab72355cc915
vasev maksim <vasvmaksim@gmail.com>
2020-11-06 17:40:36 +0300

    refactiring back

--- back/back/urls.py
+++ back/back/urls.py
@@ -1,4 +1,4 @@
-"""todo_back URL Configuration
+"""back URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
